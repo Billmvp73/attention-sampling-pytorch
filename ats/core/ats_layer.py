fix bug with simple scale normalization

ats/core/ats_layer.py
@@ -468,19 +468,26 @@ class MultiAtsParallelATSModel(nn.Module):
         attention_maps = []
         multi_patches = []
         multi_sampled_attention = []
+        ratio_scales = []
+        
         for i, (x_low, x_high, scale, attention_model) in enumerate(zip(x_lows, x_highs,self.scales, self.attention_models)):
-
+            
             # First we compute our attention map
             attention_map = attention_model(x_low)
-            
+            if scale == 1:
+                scale_num_base = attention_map.shape[1]*attention_map.shape[2]
+                ratio_scales.append(1)
+            else:
+                ratio_scales.append(attention_map.shape[1] * attention_map.shape[2]/scale_num_base)
             patches, sampled_attention = self.sampler(x_low, x_high, attention_map)
             # patches, sampled_attention = self.multiSampler(x_lows, x_highs, norm_map, max_index)
             multi_patches.append(patches)
             multi_sampled_attention.append(sampled_attention)
             attention_maps.append(attention_map)
         if self.norm_resample:
-            patches, sampled_attention, unnorm_atts = norm_resample(self.n_patches, multi_patches, multi_sampled_attention, self.scales, self.norm_atts_weight)
+            patches, sampled_attention, unnorm_atts, sampled_scales = norm_resample(self.n_patches, multi_patches, multi_sampled_attention, ratio_scales, self.norm_atts_weight)
         else:
+            sampled_scales = None
             patches = torch.cat(multi_patches, 1)
             sampled_attention = torch.cat(multi_sampled_attention, 1)
         # We compute the features of the sampled patches
@@ -505,4 +512,4 @@ class MultiAtsParallelATSModel(nn.Module):
 
         y = self.classifier(sample_features)
 
-        return y, attention_maps, patches, x_lows, patch_features
+        return y, attention_maps, patches, x_lows, patch_features, sampled_scales

ats/core/sampling.py
@@ -42,14 +42,16 @@ def norm_resample(n_samples, multi_samples, multi_attention, scales, norm_atts_w
     First normalize the sampled attention.
     """
     norm_atts = []
+    # scale[i] is the ratio of the number of patches at scale 1 to the number of patches at scale s[i]
     for i, scale in enumerate(scales):
-        norm_att_i = multi_attention[i]*scale**2
+        norm_att_i = multi_attention[i]*scale
         norm_atts.append(norm_att_i)
     # norm_atts = torch.stack(norm_atts)
     norm_atts = torch.cat(norm_atts, 1)
     unnorm_atts = torch.cat(multi_attention, 1)
     top_atts, top_ind = torch.topk(norm_atts, n_samples, 1)
     unnorm_top_atts = torch.gather(unnorm_atts, 1, top_ind)
+    sampled_scales = top_ind // n_samples
     batch_size = top_atts.shape[0]
     total_samples = torch.cat(multi_samples, 1)
     total_num, c, s0, s1 = total_samples.shape[1:]
@@ -61,8 +63,8 @@ def norm_resample(n_samples, multi_samples, multi_attention, scales, norm_atts_w
 
     # top_samples = multi_samples[top_ind]
     if norm_atts_weight:
-        return top_samples, top_atts, unnorm_top_atts # TODO: why the loss could be negative? The attentions associated with the patches are too small?
-    return top_samples, unnorm_top_atts, unnorm_top_atts
+        return top_samples, top_atts, unnorm_top_atts, sampled_scales # TODO: why the loss could be negative? The attentions associated with the patches are too small?
+    return top_samples, unnorm_top_atts, unnorm_top_atts, sampled_scales
     # Flatten the attention distribution and sampel from it
     
 